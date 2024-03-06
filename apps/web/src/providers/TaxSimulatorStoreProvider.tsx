1d6a510ec0e95e7552cb24689d495b74828bf86e
zeis974 <38920781+zeis974@users.noreply.github.com>
2024-03-06 22:31:15 +0400

    Init stores

---
+++ apps/web/src/providers/TaxSimulatorStoreProvider.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+import { type ReactNode, createContext, useRef, useContext } from "react"
+import { type StoreApi, useStore } from "zustand"
+
+import { type TaxSimulatorStore, createTaxSimulatorStore } from "@/stores/TaxSimulatorStores"
+
+export const TaxSimulatorStoreContext = createContext<StoreApi<TaxSimulatorStore> | null>(null)
+
+export interface TaxSimulatorStoreProviderProps {
+  children: ReactNode
+}
+
+export const TaxSimulatorStoreProvider = ({ children }: TaxSimulatorStoreProviderProps) => {
+  const storeRef = useRef<StoreApi<TaxSimulatorStore>>()
+  if (!storeRef.current) {
+    storeRef.current = createTaxSimulatorStore()
+  }
+
+  return (
+    <TaxSimulatorStoreContext.Provider value={storeRef.current}>
+      {children}
+    </TaxSimulatorStoreContext.Provider>
+  )
+}
+
+export const useTaxSimulatorStore = <T,>(selector: (store: TaxSimulatorStore) => T): T => {
+  const taxSimulatorStoreContext = useContext(TaxSimulatorStoreContext)
+
+  if (!taxSimulatorStoreContext) {
+    throw new Error(`${selector.name} must be used within TaxSimulatorStoreProvider`)
+  }
+
+  return useStore(taxSimulatorStoreContext, selector)
+}
---
+++ apps/web/src/stores/TaxSimulatorStores.tsx
@@ -0,0 +1,23 @@
+import type { TaxSimulatorFormLabel } from "@/services/TaxSimulator/types/TaxSimulatorTypes"
+import { createStore } from "zustand/vanilla"
+
+export type TaxState = {
+  focusInput: TaxSimulatorFormLabel | undefined
+}
+
+export type TaxActions = {
+  setFocusInput: (value: TaxState["focusInput"]) => void
+}
+
+export type TaxSimulatorStore = TaxState & TaxActions
+
+export const defaultInitState: TaxState = {
+  focusInput: undefined,
+}
+
+export const createTaxSimulatorStore = (initState: TaxState = defaultInitState) => {
+  return createStore<TaxSimulatorStore>()((set) => ({
+    ...initState,
+    setFocusInput: (value) => set({ focusInput: value }),
+  }))
+}
