76f4c27cecdc24d68985036e4ab71fd64f1b8047
zeis974 <38920781+zeis974@users.noreply.github.com>
2024-07-14 23:38:02 +0400

    Make fonts files private
    Move outside `/public`

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,37 +1,60 @@
 import "./globals.css"
 
-import localFont from "next/font/local"
 import type { Metadata } from "next"
 import { ThemeProvider } from "next-themes"
+import localFont from "next/font/local"
 
 import Navbar from "@/components/Navbar"
+import LazyMotionProvider from "@/providers/LazyMotionProvider"
+import { TaxSimulatorStoreProvider } from "@/providers/TaxSimulatorStoreProvider"
 
 export const metadata: Metadata = {
-  title: "TaxDOM - Le calculateur de taxes",
+  title: {
+    template: "%s | TaxDOM",
+    default: "TaxDOM - Le calculateur de taxes ultra marin",
+  },
   description: "...",
 }
 
 const Rowdies = localFont({
-  src: "../../public/assets/fonts/RowdiesRegular.woff2",
-  display: "swap",
+  src: "../fonts/RowdiesRegular.woff2",
   variable: "--rowdies",
+  display: "swap",
+})
+
+const NotoSansRegular = localFont({
+  src: "../fonts/NotoSansRegular.woff2",
+  variable: "--noto-sans",
+  display: "swap",
+})
+
+const NotoSansBold = localFont({
+  src: "../fonts/NotoSansBold.woff2",
+  variable: "--noto-sans-bold",
+  display: "swap",
 })
 
 export default function RootLayout({
   children,
   modal,
-}: {
-  children: React.ReactNode
-  modal: React.ReactNode
-}) {
+}: Record<"children" | "modal", React.ReactNode>) {
   return (
-    <html lang="fr" className={`${Rowdies.variable}`} suppressHydrationWarning>
+    <html
+      lang="fr"
+      className={`${Rowdies.variable} ${NotoSansRegular.variable} ${NotoSansBold.variable}`}
+      suppressHydrationWarning
+    >
       <body>
         <ThemeProvider defaultTheme="system">
-          <Navbar />
-          {children}
+          <TaxSimulatorStoreProvider>
+            <LazyMotionProvider>
+              <Navbar />
+              {children}
+              {modal}
+            </LazyMotionProvider>
+          </TaxSimulatorStoreProvider>
         </ThemeProvider>
       </body>
-    </html >
+    </html>
   )
 }
