785f747247847e4525f70f96d7f6cbcdc921b598
zeis974 <38920781+zeis974@users.noreply.github.com>
2024-12-26 21:42:53 +0400

    feat: add icons

--- apps/web/src/components/Icons/index.tsx
+++ apps/web/src/components/Icons/index.tsx
@@ -2,22 +2,28 @@ import AddIcon from "./AddIcon"
 import ChevronIcon from "./ChevronIcon"
 import DownloadIcon from "./DownloadIcon"
 import InfoIcon from "./InfoIcon"
+import ListIcon from "./ListIcon"
 import LoadingIcon from "./LoadingIcon"
+import MagicIcon from "./MagicIcon"
 import ParcelIcon from "./ParcelIcon"
 import SearchIcon from "./SearchIcon"
 import SettingIcon from "./SettingsIcon"
 import ShareIcon from "./ShareIcon"
 import TaxIcon from "./TaxIcon"
+import TaxDOMLogo from "./TaxDOMLogo"
 
 export {
   AddIcon,
   ChevronIcon,
   DownloadIcon,
   InfoIcon,
+  ListIcon,
   LoadingIcon,
+  MagicIcon,
   ParcelIcon,
   SearchIcon,
   SettingIcon,
   ShareIcon,
   TaxIcon,
+  TaxDOMLogo,
 }
