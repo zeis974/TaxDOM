13ba04ce5ab3d5b3c70b14c8f5ff34d3c82496e0
zeis974 <38920781+zeis974@users.noreply.github.com>
2025-04-27 22:31:04 +0400

    cache template data for `24 hours`

--- apps/web/src/lib/fetchTemplate.ts
+++ apps/web/src/lib/fetchTemplate.ts
@@ -3,6 +3,7 @@ import { queryOptions } from "@tanstack/react-query"
 
 export const fetchTemplates = queryOptions({
   queryKey: ["templates"],
+  staleTime: 24 * 60 * 60 * 1000, // 24 hours
   queryFn: async () => {
     const res: ParcelSimulatorTemplateType & Response = await fetch(
       `${process.env.NEXT_PUBLIC_API_URL}/simulator/templates`,
