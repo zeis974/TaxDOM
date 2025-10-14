625dc044404cdf6f7f6b69904cb1012284e710e9
zeis974 <38920781+zeis974@users.noreply.github.com>
2025-10-14 18:57:06 +0400

    feat: add typed routes

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from "next"
-
 import path from "node:path"
 import url from "node:url"
 
 const nextConfig: NextConfig = {
   output: "standalone",
   outputFileTracingRoot: path.join(url.fileURLToPath(new URL(".", import.meta.url)), "../../"),
+  typedRoutes: true,
 }
 
 export default nextConfig
