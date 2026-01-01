45ab6f4d084c6092a22e45f6832fe7778643372c
zeis974 <38920781+zeis974@users.noreply.github.com>
2026-01-01 12:52:34 +0400

    feat: remote images

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -6,6 +6,9 @@ const nextConfig: NextConfig = {
   output: "standalone",
   outputFileTracingRoot: path.join(url.fileURLToPath(new URL(".", import.meta.url)), "../../"),
   typedRoutes: true,
+  images: {
+    remotePatterns: [new URL("https://r2.taxdom.re/images/**")],
+  },
 }
 
 export default nextConfig
