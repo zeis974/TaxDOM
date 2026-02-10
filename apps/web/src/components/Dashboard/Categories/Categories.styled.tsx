8fdbde4718bc140504b2c257a6e0f7acd3011a48
zeis974 <38920781+zeis974@users.noreply.github.com>
2026-02-10 21:11:27 +0400

    ref: categories component with optimistic updates

--- apps/web/src/components/Dashboard/Categories/Categories.styled.tsx
+++ apps/web/src/components/Dashboard/Categories/Categories.styled.tsx
@@ -2,6 +2,7 @@ import { styled } from "@/panda/jsx"
 
 export const Container = styled.div`
   color: token(colors.primary);
+  font-family: token(fonts.nativeFont);
   height: 100%;
   display: flex;
   flex-direction: column;
@@ -11,10 +12,32 @@ export const Header = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin-bottom: 24px;
+  padding-bottom: 24px;
+  background: token(colors.background);
+  position: sticky;
+  top: 0;
+  z-index: 10;
+`
 
-  & h1 {
-    font-weight: bold;
-    font-family: token(fonts.nativeFont);
+export const HeaderTitle = styled.div`
+  display: flex;
+  align-items: end;
+  gap: 15px;
+
+  & h2 {
+    margin: 0;
   }
+
+  & span {
+    color: token(colors.darkGray);
+    font-size: 14px;
+    font-weight: 500;
+  }
+`
+
+export const HeaderActions = styled.div`
+  display: flex;
+  height: 100%;
+  align-items: center;
+  gap: 10px;
 `
