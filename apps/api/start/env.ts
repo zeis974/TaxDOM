479f026f88ecee137e5ec83c82110c7fe16cfe5f
zeis974 <38920781+zeis974@users.noreply.github.com>
2026-04-11 19:48:30 +0400

    feat: use session instead of `API_KEY`

--- apps/api/start/env.ts
+++ apps/api/start/env.ts
@@ -14,7 +14,6 @@ import { Env } from "@adonisjs/core/env"
 export default await Env.create(new URL("../", import.meta.url), {
   NODE_ENV: Env.schema.enum(["development", "production", "test"] as const),
   PORT: Env.schema.number(),
-  API_KEY: Env.schema.secret(),
   APP_KEY: Env.schema.secret(),
   HOST: Env.schema.string({ format: "host" }),
   LOG_LEVEL: Env.schema.enum(["fatal", "error", "warn", "info", "debug", "trace"]),
--- apps/web/src/actions/searchProducts.ts
+++ apps/web/src/actions/searchProducts.ts
@@ -3,9 +3,6 @@
 export async function searchProducts(value: string) {
   const res = await fetch(`${process.env.API_URL}/products/search?name=${value}`, {
     method: "GET",
-    headers: {
-      Authorization: `Bearer ${process.env.API_KEY}`,
-    },
   }).then((res) => res.json())
 
   return res
