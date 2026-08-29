c38e63a67c938b9e9dcb89538dfd34e288ac8079
zeis974 <38920781+zeis974@users.noreply.github.com>
2026-08-29 14:37:53 +0400

    feat(assets): map web flag codes to bundled asset URLs

---
+++ apps/web/src/assets/flags/index.ts
@@ -0,0 +1,391 @@
+import flag_ad from "./ad.svg"
+import flag_ae from "./ae.svg"
+import flag_ag from "./ag.svg"
+import flag_al from "./al.svg"
+import flag_am from "./am.svg"
+import flag_ao from "./ao.svg"
+import flag_ar from "./ar.svg"
+import flag_at from "./at.svg"
+import flag_au from "./au.svg"
+import flag_az from "./az.svg"
+import flag_ba from "./ba.svg"
+import flag_bb from "./bb.svg"
+import flag_bd from "./bd.svg"
+import flag_be from "./be.svg"
+import flag_bf from "./bf.svg"
+import flag_bg from "./bg.svg"
+import flag_bh from "./bh.svg"
+import flag_bi from "./bi.svg"
+import flag_bj from "./bj.svg"
+import flag_bn from "./bn.svg"
+import flag_bo from "./bo.svg"
+import flag_br from "./br.svg"
+import flag_bs from "./bs.svg"
+import flag_bt from "./bt.svg"
+import flag_bw from "./bw.svg"
+import flag_by from "./by.svg"
+import flag_bz from "./bz.svg"
+import flag_ca from "./ca.svg"
+import flag_cd from "./cd.svg"
+import flag_cf from "./cf.svg"
+import flag_cg from "./cg.svg"
+import flag_ch from "./ch.svg"
+import flag_ci from "./ci.svg"
+import flag_cl from "./cl.svg"
+import flag_cm from "./cm.svg"
+import flag_cn from "./cn.svg"
+import flag_co from "./co.svg"
+import flag_cr from "./cr.svg"
+import flag_cu from "./cu.svg"
+import flag_cv from "./cv.svg"
+import flag_cy from "./cy.svg"
+import flag_cz from "./cz.svg"
+import flag_de from "./de.svg"
+import flag_dj from "./dj.svg"
+import flag_dk from "./dk.svg"
+import flag_dm from "./dm.svg"
+import flag_do from "./do.svg"
+import flag_dz from "./dz.svg"
+import flag_ec from "./ec.svg"
+import flag_ee from "./ee.svg"
+import flag_eg from "./eg.svg"
+import flag_er from "./er.svg"
+import flag_es from "./es.svg"
+import flag_et from "./et.svg"
+import flag_fi from "./fi.svg"
+import flag_fj from "./fj.svg"
+import flag_fm from "./fm.svg"
+import flag_fr from "./fr.svg"
+import flag_ga from "./ga.svg"
+import flag_gb from "./gb.svg"
+import flag_gd from "./gd.svg"
+import flag_ge from "./ge.svg"
+import flag_gh from "./gh.svg"
+import flag_gm from "./gm.svg"
+import flag_gn from "./gn.svg"
+import flag_gq from "./gq.svg"
+import flag_gr from "./gr.svg"
+import flag_gt from "./gt.svg"
+import flag_gw from "./gw.svg"
+import flag_gy from "./gy.svg"
+import flag_hn from "./hn.svg"
+import flag_hr from "./hr.svg"
+import flag_ht from "./ht.svg"
+import flag_hu from "./hu.svg"
+import flag_id from "./id.svg"
+import flag_ie from "./ie.svg"
+import flag_il from "./il.svg"
+import flag_in from "./in.svg"
+import flag_iq from "./iq.svg"
+import flag_ir from "./ir.svg"
+import flag_is from "./is.svg"
+import flag_it from "./it.svg"
+import flag_jm from "./jm.svg"
+import flag_jo from "./jo.svg"
+import flag_jp from "./jp.svg"
+import flag_ke from "./ke.svg"
+import flag_kg from "./kg.svg"
+import flag_kh from "./kh.svg"
+import flag_ki from "./ki.svg"
+import flag_km from "./km.svg"
+import flag_kn from "./kn.svg"
+import flag_kp from "./kp.svg"
+import flag_kr from "./kr.svg"
+import flag_kw from "./kw.svg"
+import flag_kz from "./kz.svg"
+import flag_la from "./la.svg"
+import flag_lb from "./lb.svg"
+import flag_lc from "./lc.svg"
+import flag_li from "./li.svg"
+import flag_lk from "./lk.svg"
+import flag_lr from "./lr.svg"
+import flag_ls from "./ls.svg"
+import flag_lt from "./lt.svg"
+import flag_lu from "./lu.svg"
+import flag_lv from "./lv.svg"
+import flag_ly from "./ly.svg"
+import flag_ma from "./ma.svg"
+import flag_mc from "./mc.svg"
+import flag_md from "./md.svg"
+import flag_me from "./me.svg"
+import flag_mg from "./mg.svg"
+import flag_mk from "./mk.svg"
+import flag_ml from "./ml.svg"
+import flag_mm from "./mm.svg"
+import flag_mn from "./mn.svg"
+import flag_mr from "./mr.svg"
+import flag_mt from "./mt.svg"
+import flag_mu from "./mu.svg"
+import flag_mv from "./mv.svg"
+import flag_mw from "./mw.svg"
+import flag_mx from "./mx.svg"
+import flag_my from "./my.svg"
+import flag_mz from "./mz.svg"
+import flag_na from "./na.svg"
+import flag_ne from "./ne.svg"
+import flag_ng from "./ng.svg"
+import flag_ni from "./ni.svg"
+import flag_nl from "./nl.svg"
+import flag_no from "./no.svg"
+import flag_np from "./np.svg"
+import flag_nr from "./nr.svg"
+import flag_nz from "./nz.svg"
+import flag_om from "./om.svg"
+import flag_pa from "./pa.svg"
+import flag_pe from "./pe.svg"
+import flag_pg from "./pg.svg"
+import flag_ph from "./ph.svg"
+import flag_pk from "./pk.svg"
+import flag_pl from "./pl.svg"
+import flag_ps from "./ps.svg"
+import flag_pt from "./pt.svg"
+import flag_pw from "./pw.svg"
+import flag_py from "./py.svg"
+import flag_qa from "./qa.svg"
+import flag_ro from "./ro.svg"
+import flag_rs from "./rs.svg"
+import flag_ru from "./ru.svg"
+import flag_rw from "./rw.svg"
+import flag_sa from "./sa.svg"
+import flag_sb from "./sb.svg"
+import flag_sc from "./sc.svg"
+import flag_sd from "./sd.svg"
+import flag_se from "./se.svg"
+import flag_sg from "./sg.svg"
+import flag_si from "./si.svg"
+import flag_sk from "./sk.svg"
+import flag_sl from "./sl.svg"
+import flag_sm from "./sm.svg"
+import flag_sn from "./sn.svg"
+import flag_so from "./so.svg"
+import flag_sr from "./sr.svg"
+import flag_ss from "./ss.svg"
+import flag_st from "./st.svg"
+import flag_sv from "./sv.svg"
+import flag_sy from "./sy.svg"
+import flag_sz from "./sz.svg"
+import flag_td from "./td.svg"
+import flag_tg from "./tg.svg"
+import flag_th from "./th.svg"
+import flag_tj from "./tj.svg"
+import flag_tl from "./tl.svg"
+import flag_tm from "./tm.svg"
+import flag_tn from "./tn.svg"
+import flag_to from "./to.svg"
+import flag_tr from "./tr.svg"
+import flag_tt from "./tt.svg"
+import flag_tv from "./tv.svg"
+import flag_tz from "./tz.svg"
+import flag_ua from "./ua.svg"
+import flag_ug from "./ug.svg"
+import flag_us from "./us.svg"
+import flag_uy from "./uy.svg"
+import flag_uz from "./uz.svg"
+import flag_va from "./va.svg"
+import flag_vc from "./vc.svg"
+import flag_ve from "./ve.svg"
+import flag_vn from "./vn.svg"
+import flag_vu from "./vu.svg"
+import flag_ws from "./ws.svg"
+import flag_xk from "./xk.svg"
+import flag_ye from "./ye.svg"
+import flag_za from "./za.svg"
+import flag_zm from "./zm.svg"
+import flag_zw from "./zw.svg"
+
+export const FLAGS_BY_CODE: Record<string, string> = {
+  ad: flag_ad.src,
+  ae: flag_ae.src,
+  ag: flag_ag.src,
+  al: flag_al.src,
+  am: flag_am.src,
+  ao: flag_ao.src,
+  ar: flag_ar.src,
+  at: flag_at.src,
+  au: flag_au.src,
+  az: flag_az.src,
+  ba: flag_ba.src,
+  bb: flag_bb.src,
+  bd: flag_bd.src,
+  be: flag_be.src,
+  bf: flag_bf.src,
+  bg: flag_bg.src,
+  bh: flag_bh.src,
+  bi: flag_bi.src,
+  bj: flag_bj.src,
+  bn: flag_bn.src,
+  bo: flag_bo.src,
+  br: flag_br.src,
+  bs: flag_bs.src,
+  bt: flag_bt.src,
+  bw: flag_bw.src,
+  by: flag_by.src,
+  bz: flag_bz.src,
+  ca: flag_ca.src,
+  cd: flag_cd.src,
+  cf: flag_cf.src,
+  cg: flag_cg.src,
+  ch: flag_ch.src,
+  ci: flag_ci.src,
+  cl: flag_cl.src,
+  cm: flag_cm.src,
+  cn: flag_cn.src,
+  co: flag_co.src,
+  cr: flag_cr.src,
+  cu: flag_cu.src,
+  cv: flag_cv.src,
+  cy: flag_cy.src,
+  cz: flag_cz.src,
+  de: flag_de.src,
+  dj: flag_dj.src,
+  dk: flag_dk.src,
+  dm: flag_dm.src,
+  do: flag_do.src,
+  dz: flag_dz.src,
+  ec: flag_ec.src,
+  ee: flag_ee.src,
+  eg: flag_eg.src,
+  er: flag_er.src,
+  es: flag_es.src,
+  et: flag_et.src,
+  fi: flag_fi.src,
+  fj: flag_fj.src,
+  fm: flag_fm.src,
+  fr: flag_fr.src,
+  ga: flag_ga.src,
+  gb: flag_gb.src,
+  gd: flag_gd.src,
+  ge: flag_ge.src,
+  gh: flag_gh.src,
+  gm: flag_gm.src,
+  gn: flag_gn.src,
+  gq: flag_gq.src,
+  gr: flag_gr.src,
+  gt: flag_gt.src,
+  gw: flag_gw.src,
+  gy: flag_gy.src,
+  hn: flag_hn.src,
+  hr: flag_hr.src,
+  ht: flag_ht.src,
+  hu: flag_hu.src,
+  id: flag_id.src,
+  ie: flag_ie.src,
+  il: flag_il.src,
+  in: flag_in.src,
+  iq: flag_iq.src,
+  ir: flag_ir.src,
+  is: flag_is.src,
+  it: flag_it.src,
+  jm: flag_jm.src,
+  jo: flag_jo.src,
+  jp: flag_jp.src,
+  ke: flag_ke.src,
+  kg: flag_kg.src,
+  kh: flag_kh.src,
+  ki: flag_ki.src,
+  km: flag_km.src,
+  kn: flag_kn.src,
+  kp: flag_kp.src,
+  kr: flag_kr.src,
+  kw: flag_kw.src,
+  kz: flag_kz.src,
+  la: flag_la.src,
+  lb: flag_lb.src,
+  lc: flag_lc.src,
+  li: flag_li.src,
+  lk: flag_lk.src,
+  lr: flag_lr.src,
+  ls: flag_ls.src,
+  lt: flag_lt.src,
+  lu: flag_lu.src,
+  lv: flag_lv.src,
+  ly: flag_ly.src,
+  ma: flag_ma.src,
+  mc: flag_mc.src,
+  md: flag_md.src,
+  me: flag_me.src,
+  mg: flag_mg.src,
+  mk: flag_mk.src,
+  ml: flag_ml.src,
+  mm: flag_mm.src,
+  mn: flag_mn.src,
+  mr: flag_mr.src,
+  mt: flag_mt.src,
+  mu: flag_mu.src,
+  mv: flag_mv.src,
+  mw: flag_mw.src,
+  mx: flag_mx.src,
+  my: flag_my.src,
+  mz: flag_mz.src,
+  na: flag_na.src,
+  ne: flag_ne.src,
+  ng: flag_ng.src,
+  ni: flag_ni.src,
+  nl: flag_nl.src,
+  no: flag_no.src,
+  np: flag_np.src,
+  nr: flag_nr.src,
+  nz: flag_nz.src,
+  om: flag_om.src,
+  pa: flag_pa.src,
+  pe: flag_pe.src,
+  pg: flag_pg.src,
+  ph: flag_ph.src,
+  pk: flag_pk.src,
+  pl: flag_pl.src,
+  ps: flag_ps.src,
+  pt: flag_pt.src,
+  pw: flag_pw.src,
+  py: flag_py.src,
+  qa: flag_qa.src,
+  ro: flag_ro.src,
+  rs: flag_rs.src,
+  ru: flag_ru.src,
+  rw: flag_rw.src,
+  sa: flag_sa.src,
+  sb: flag_sb.src,
+  sc: flag_sc.src,
+  sd: flag_sd.src,
+  se: flag_se.src,
+  sg: flag_sg.src,
+  si: flag_si.src,
+  sk: flag_sk.src,
+  sl: flag_sl.src,
+  sm: flag_sm.src,
+  sn: flag_sn.src,
+  so: flag_so.src,
+  sr: flag_sr.src,
+  ss: flag_ss.src,
+  st: flag_st.src,
+  sv: flag_sv.src,
+  sy: flag_sy.src,
+  sz: flag_sz.src,
+  td: flag_td.src,
+  tg: flag_tg.src,
+  th: flag_th.src,
+  tj: flag_tj.src,
+  tl: flag_tl.src,
+  tm: flag_tm.src,
+  tn: flag_tn.src,
+  to: flag_to.src,
+  tr: flag_tr.src,
+  tt: flag_tt.src,
+  tv: flag_tv.src,
+  tz: flag_tz.src,
+  ua: flag_ua.src,
+  ug: flag_ug.src,
+  us: flag_us.src,
+  uy: flag_uy.src,
+  uz: flag_uz.src,
+  va: flag_va.src,
+  vc: flag_vc.src,
+  ve: flag_ve.src,
+  vn: flag_vn.src,
+  vu: flag_vu.src,
+  ws: flag_ws.src,
+  xk: flag_xk.src,
+  ye: flag_ye.src,
+  za: flag_za.src,
+  zm: flag_zm.src,
+  zw: flag_zw.src,
+}
